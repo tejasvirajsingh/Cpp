b1b43645cb299d4a9e004ae16b5fec8018d147d3
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-08-31 14:48:41 +0530

    First code in C++

---
+++ 1st.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Add the numbers
+    sum = num1 + num2;
+
+    // Display the result
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+
+    return 0;
+}
