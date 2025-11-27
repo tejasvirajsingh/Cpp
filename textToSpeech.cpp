8e79097214cea4a0919001b1b3b2df1de3b7045e
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-11-27 23:54:16 +0530

    updatedd

--- textToSpeech.cpp
+++ textToSpeech.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
 #include <windows.h>
-#include <sapi.h>      // 🔥 IMPORTANT
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 using namespace std;
 
 int main() {
-    // Initialize COM
-    HRESULT hr = CoInitialize(NULL);
-    if (FAILED(hr)) {
-        cout << "COM initialization failed!" << endl;
-        return -1;
-    }
-
-    // Create voice object
-    ISpVoice* pVoice = NULL;
-    hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL,
-                          IID_ISpVoice, (void**)&pVoice);
-
-    if (SUCCEEDED(hr)) {
-        pVoice->Speak(L"Haa... bolo Pandey ji?", SPF_IS_XML, NULL);
-        pVoice->Release();
-        pVoice = NULL;
-    } else {
-        cout << "Failed to create voice instance!" << endl;
-    }
-
-    // Cleanup
-    CoUninitialize();
+    PlaySound(TEXT("sound.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    cout << " MR Max Kaya Haii..." << endl;
     return 0;
 }
