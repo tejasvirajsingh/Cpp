1902722bff4ab2d9236d5465cb7c977da08c649e
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-29 13:53:02 +0530

    some tricky question done

---
+++ 6A7/Typeconverstion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    // int a = 20;
+    // float b = a;
+
+    // cout << b ;
+
+    // int a = 3;
+    // float b = 16;
+    // float c = b / a;
+    // cout << c << endl; // 5.333
+
+    // string a = "Tej";
+    // string b = "_Rajput";
+
+    //   string name = a +b;  // Tej_Rajput
+
+    // cout << name << endl;
+
+        return 0;
+}
