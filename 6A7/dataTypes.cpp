abc473511f17338594ff7aab36d68c33ed21306c
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-29 13:06:32 +0530

    dataTypes

---
+++ 6A7/dataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    float b = 5.5;
+    double c = 99.99;
+    char d = 'A';
+    bool e = true;
+
+    cout << "Primitive Data Types:" << endl;
+    cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+
+    return 0;
+}
