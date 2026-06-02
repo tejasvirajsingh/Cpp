2377423872939952ea2e0fbac580a318c16de12b
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-06-02 08:56:20 +0530

    binary

---
+++ Practice/binarySearch.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target)
+{
+    int low = 0;
+    int high = size - 1;
+
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == target)
+        {
+            return mid;
+        }
+        else if(arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {3, 5, 6, 7, 8, 10,45};
+    int target = 8;
+
+      int result = binarySearch(arr, 6, target);
+
+    
+        cout << "The index number is "<< result;
+
+    return 0;
+}
