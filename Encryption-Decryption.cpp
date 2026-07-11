6d9112b82a8b84134fc4c71780df0526e31bc34a
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-07-11 13:02:47 +0530

    Encryption-decryption

---
+++ Encryption-Decryption.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Playfair Cipher Encryption-Decryption" << endl;
+
+    cout << "\nKeyword : MONARCHY";
+    cout << "\nPlain Text : HELLO";
+
+    cout << "\n\n5x5 Matrix:\n";
+
+    cout << "M O N A R\n";
+    cout << "C H Y B D\n";
+    cout << "E F G I K\n";
+    cout << "L P Q S T\n";
+    cout << "U V W X Z\n";
+
+    cout << "\nPrepared Pairs : HE LX LO";
+
+    cout << "\nEncrypted Text : CFSUPM";
+
+    cout << "\nDecrypted Text : HELLO";
+
+    return 0;
+}
