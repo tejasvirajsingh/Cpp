8d63e7651f712915a8088c92bf5c22d8999dbbea
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-07-21 00:52:57 +0530

    Cpp

--- Practice/binarySearch.cpp
+++ Practice/binarySearch.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int binarySearch(int arr[], int size, int target)
@@ -6,15 +6,15 @@ int binarySearch(int arr[], int size, int target)
     int low = 0;
     int high = size - 1;
 
-    while(low <= high)
+    while (low <= high)
     {
         int mid = low + (high - low) / 2;
 
-        if(arr[mid] == target)
+        if (arr[mid] == target)
         {
             return mid;
         }
-        else if(arr[mid] < target)
+        else if (arr[mid] < target)
         {
             low = mid + 1;
         }
@@ -29,13 +29,12 @@ int binarySearch(int arr[], int size, int target)
 
 int main()
 {
-    int arr[] = {3, 5, 6, 7, 8, 10,45};
+    int arr[] = {3, 5, 6, 7, 8, 10, 45};
     int target = 8;
 
-      int result = binarySearch(arr, 6, target);
+    int result = binarySearch(arr, 6, target);
 
-    
-        cout << "The index number is "<< result;
+    cout << "The index number is " << result;
 
     return 0;
 }
