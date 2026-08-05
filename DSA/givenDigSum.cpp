ec0bea4deb2f7198504cc326d5ed51a2be735741
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-08-06 01:28:10 +0530

    New DSA Folder

---
+++ DSA/givenDigSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) {
+
+       
+
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    int num;
+
+    cout << "Enter number: ";
+    cin >> num;
+
+    cout << "Answer: " << obj.addDigits(num);
+
+    return 0;
+}
