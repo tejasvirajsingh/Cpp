b93acd5bd4416a1fad40d399f89d585592fa6148
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-08-08 01:21:21 +0530

    Stack

---
+++ Stack/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+
+    stack<int> st;
+
+    st.push(10);
+    st.push(39);
+    st.push(32);
+    st.push(22);
+    st.push(55);
+    st.push(98);
+    st.push(44);
+
+    st.pop();
+
+    cout << st.size() << endl;
+    cout << st.top();
+}
